73341873148d0d47efdbf336ffc8a9c3bc1ccc14
Praveen Kumar Verma <pkv121@gmail.com>
2017-10-08 16:47:58 +0530

    Added model on admin site

--- Autotriage/admin.py
+++ Autotriage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from Autotriage.models import Employee, CompanyMaster, CompanyServer
 
-# Register your models here.
+admin.site.register(Employee);
+admin.site.register(CompanyMaster)
+admin.site.register(CompanyServer)
